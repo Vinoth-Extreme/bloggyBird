958ea24ab06f3f3721c0c753d91db8ce5e090fdb
Vinoth V <67054923+Vinoth-Extreme@users.noreply.github.com>
2021-09-26 21:37:00 +0530

    Update settings.py

--- bloggy/bloggy/settings.py
+++ bloggy/bloggy/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bloggy',
         'USER': 'postgres',
-        'PASSWORD': 'Vinoth2001',
+        'PASSWORD': 'Password',
         'HOST': 'localhost'
     }
 }
